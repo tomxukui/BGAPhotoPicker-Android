b5d387982909b5121c14ff2874925e89c226c682
tom <152780382@qq.com>
2018-06-08 19:32:31 +0800

    v2.0.6

--- library/src/main/java/cn/bingoogolapple/photopicker/imageloader/BGAGlideImageLoader.java
+++ library/src/main/java/cn/bingoogolapple/photopicker/imageloader/BGAGlideImageLoader.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2016 bingoogolapple
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cn.bingoogolapple.photopicker.imageloader;
 
 import android.app.Activity;
@@ -44,26 +29,37 @@ public class BGAGlideImageLoader extends BGAImageLoader {
     public void display(final ImageView imageView, String path, @DrawableRes int loadingResId, @DrawableRes int failResId, int width, int height, final DisplayDelegate delegate) {
         final String finalPath = getPath(path);
         Activity activity = getActivity(imageView);
-        Glide.with(activity).load(finalPath).apply(new RequestOptions().placeholder(loadingResId).error(failResId).override(width, height).dontAnimate()).listener(new RequestListener<Drawable>() {
-            @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                return false;
-            }
 
-            @Override
-            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                if (delegate != null) {
-                    delegate.onSuccess(imageView, finalPath);
-                }
-                return false;
-            }
-        }).into(imageView);
+        if (isOk(activity)) {
+            Glide.with(activity)
+                    .load(finalPath)
+                    .apply(new RequestOptions().placeholder(loadingResId).error(failResId).override(width, height).dontAnimate())
+                    .listener(new RequestListener<Drawable>() {
+
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            if (delegate != null) {
+                                delegate.onSuccess(imageView, finalPath);
+                            }
+                            return false;
+                        }
+
+                    })
+                    .into(imageView);
+        }
     }
 
     @Override
     public void download(String path, final DownloadDelegate delegate) {
         final String finalPath = getPath(path);
+
         Glide.with(BGABaseAdapterUtil.getApp()).asBitmap().load(finalPath).into(new SimpleTarget<Bitmap>() {
+
             @Override
             public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
                 if (delegate != null) {
@@ -77,16 +73,26 @@ public class BGAGlideImageLoader extends BGAImageLoader {
                     delegate.onFailed(finalPath);
                 }
             }
+
         });
+    }
+
+    private boolean isOk(Activity activity) {
+        return activity != null && !activity.isFinishing();
     }
 
     @Override
     public void pause(Activity activity) {
-        Glide.with(activity).pauseRequests();
+        if (isOk(activity)) {
+            Glide.with(activity).pauseRequests();
+        }
     }
 
     @Override
     public void resume(Activity activity) {
-        Glide.with(activity).resumeRequestsRecursive();
+        if (isOk(activity)) {
+            Glide.with(activity).resumeRequestsRecursive();
+        }
     }
+
 }
