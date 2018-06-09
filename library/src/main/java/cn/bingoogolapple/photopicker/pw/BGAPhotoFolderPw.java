2d52e508940947ea84caad7001e97fd9d168c53b
tom <152780382@qq.com>
2018-06-09 10:27:53 +0800

    v3.0.0

--- library/src/main/java/cn/bingoogolapple/photopicker/pw/BGAPhotoFolderPw.java
+++ library/src/main/java/cn/bingoogolapple/photopicker/pw/BGAPhotoFolderPw.java
@@ -152,7 +152,7 @@ public class BGAPhotoFolderPw extends BGABasePopupWindow implements BGAOnRVItemC
         protected void fillData(BGAViewHolderHelper helper, int position, BGAPhotoFolderModel model) {
             helper.setText(R.id.tv_item_photo_folder_name, model.name);
             helper.setText(R.id.tv_item_photo_folder_count, String.valueOf(model.getCount()));
-            BGAImage.display(helper.getImageView(R.id.iv_item_photo_folder_photo), R.mipmap.bga_pp_ic_holder_dark, model.coverPath, mImageSize);
+            BGAImage.display(helper.getImageView(R.id.iv_item_photo_folder_photo), R.mipmap.bga_pp_ic_holder_light, model.coverPath, mImageSize);
         }
 
     }
