78c7456943498bbee704e1897d6390b6c7882a8d
xukui <152780382@qq.com>
2018-10-24 18:43:46 +0800

    1.修改界面

--- library/src/main/java/cn/bingoogolapple/photopicker/model/BGAPhotoFolderModel.java
+++ library/src/main/java/cn/bingoogolapple/photopicker/model/BGAPhotoFolderModel.java
@@ -1,28 +1,9 @@
-/**
- * Copyright 2016 bingoogolapple
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cn.bingoogolapple.photopicker.model;
 
 import java.util.ArrayList;
 
-/**
- * 作者:王浩 邮件:bingoogolapple@gmail.com
- * 创建时间:16/6/24 下午6:18
- * 描述:
- */
 public class BGAPhotoFolderModel {
+
     public String name;
     public String coverPath;
     private ArrayList<String> mPhotos = new ArrayList<>();
@@ -30,8 +11,8 @@ public class BGAPhotoFolderModel {
 
     public BGAPhotoFolderModel(boolean takePhotoEnabled) {
         mTakePhotoEnabled = takePhotoEnabled;
-        if (takePhotoEnabled) {
-            // 拍照
+
+        if (takePhotoEnabled) {//拍照
             mPhotos.add("");
         }
     }
@@ -56,4 +37,5 @@ public class BGAPhotoFolderModel {
     public int getCount() {
         return mTakePhotoEnabled ? mPhotos.size() - 1 : mPhotos.size();
     }
+
 }
