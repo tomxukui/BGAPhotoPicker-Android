800c6c360d6977759be313b768c22b1377045b6b
xukui <152780382@qq.com>
2018-10-24 19:35:20 +0800

    1.修改界面

--- demo/src/main/java/cn/bingoogolapple/photopicker/demo/activity/MainActivity.java
+++ demo/src/main/java/cn/bingoogolapple/photopicker/demo/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import cn.bingoogolapple.photopicker.demo.R;
 
@@ -15,23 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.tv_moment_list_add).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_circle).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MomentAddActivity.class)));
 
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MomentAddActivity.class));
-            }
-
-        });
-
-        findViewById(R.id.tv_moment_list_system).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SystemGalleryActivity.class));
-            }
-
-        });
+        findViewById(R.id.btn_system).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SystemGalleryActivity.class)));
     }
 
 }
